4bacee1d535c6b5190e5e0f2553912f0df8bf524
Maximilian Körner <maximilian.koerner@gmx.com>
2014-01-11 14:54:35 +0100

    fast* = max

--- fastController.py
+++ fastController.py
@@ -1,13 +1,13 @@
 from threading import Thread
 from fastAudioController import AudioController
-from fastLeapController import NewLeapController
+from fastLeapController import fastLeapController
 
 class fastController(Thread):
     def __init__(self, filePath):
         Thread.__init__(self)
-        self.leap = NewLeapController()
+        self.leap = fastLeapController()
         self.leftSampler = AudioController(filePath, self.leap.getScale)
         self.leap.start()
 
 if __name__ == "__main__":
-    c = fastController( "input/scratch.wav","input/scratch.wav")
+    c = fastController( "input/scratch.wav")
