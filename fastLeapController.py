38776940bce9ea147afab12d840fb150d9f812e1
Maximilian Körner <maximilian.koerner@gmx.com>
2014-02-04 12:45:10 +0100

    changed Interaktion areas

--- fastLeapController.py
+++ fastLeapController.py
@@ -37,15 +37,10 @@ class fastLeapController(Leap.Listener):
             return min(frame.hands.rightmost.fingers, key=lambda finger: finger.tip_position.y).tip_position
         return frame.hands.rightmost.palm_position
 
-
     def getTranslation(self, frame):
         if self.lastFrame:
             return frame.translation(self.lastFrame), frame.translation_probability(self.lastFrame)
         return None, None
-
-    # def on_frame(self, controller):
-    #     self.path.append(controller.frame())
-
 
     def on_frame(self, controller):
         frame = controller.frame()
@@ -55,7 +50,7 @@ class fastLeapController(Leap.Listener):
         if len(gestures) > 0:
             if gestures[0].type == Leap.Gesture.TYPE_CIRCLE:
                 circle = Leap.CircleGesture(gestures[0])
-                if circle.center[0] < 0:
+                if circle.center[1] > 250:
                     self.volume += 1 if circle.pointables[0].direction.angle_to(circle.normal) <= math.pi / 2 else -1
 
                     if self.volume > 100:
@@ -68,7 +63,6 @@ class fastLeapController(Leap.Listener):
         self.path.append(frame)
 
     def getScale(self):
-
         #pop next frame if available, else returns old scale
         frame = self.path.pop() if len(self.path) > 0 else None
         if not frame:
@@ -82,14 +76,12 @@ class fastLeapController(Leap.Listener):
         self.lastFrame = frame
 
         #slowdoooooown
-        if pos and pos.y < 150 and pos.x > 0:
-            scale = translation.x / 3
-        elif pos and pos.y < 250 and pos.x > 0:
+        if pos and pos.y < 150:
+            scale = translation.x / 4
+        elif pos and pos.y < 250:
             scale = (pos.y - 150) / 100
         else:
             scale = 1
-
-        # scale = translation.x / 3 if pos and pos.y < 200 else 1.0
 
         self.path.clear()
 
